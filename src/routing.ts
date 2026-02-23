f1fdd3fbc09d32d607b9ea22c8a87bb609c41b2d
tinoe0404 <tinoe044@gmail.com>
2026-02-23 12:18:39 +0200

    feat: Implement internationalization routing with next-intl for English and Zulu locales.

--- src/routing.ts
+++ src/routing.ts
@@ -1,5 +1,4 @@
 import { defineRouting } from 'next-intl/routing';
-import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
     locales: ['en', 'zu'],
@@ -8,6 +7,3 @@ export const routing = defineRouting({
 });
 
 export type Locale = (typeof routing.locales)[number];
-
-export const { Link, redirect, usePathname, useRouter } =
-    createNavigation(routing);
